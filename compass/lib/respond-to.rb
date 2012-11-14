3fc5a2d247ad8f67e266b058e6ec203971822d83
Ian Carrico <Ian@FourKitchens.com>
2012-11-14 12:49:00 -0800

    Update the code to allow a optional no-query class in the list

--- compass/lib/respond-to.rb
+++ compass/lib/respond-to.rb
@@ -3,3 +3,16 @@ require 'breakpoint'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('respond-to', :path => extension_path)
+
+module Sass::Script::Functions
+  def is_css_class(string)
+    assert_type string, :String
+    Sass::Script::Bool.new(string.start_with?('.'))
+  end
+  def is_css_id(string)
+    assert_type string, :String
+    Sass::Script::Bool.new(string.start_with?('#'))
+  end
+  declare :is_css_class, :args => [:string]
+  declare :is_css_id, :args => [:string]
+end
